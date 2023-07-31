6dd5c5ef11ed6afb17e6a6b35e3efc282c39a7cc
ManOfGod14 <maliki.tcherou1@gmail.com>
2023-07-31 13:44:37 +0100

    updated 20230731 1344

--- nodejs/readline-and-events/divisionApp.js
+++ nodejs/readline-and-events/divisionApp.js
@@ -2,5 +2,5 @@
  * Exercice
  * créér une application en utilisant readline de node
  * qui prends en entrée deux nombres et afficher
- * le reusltat de leur division
+ * le resultat de leur division
  */
